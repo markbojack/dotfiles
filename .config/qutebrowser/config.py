c83a239a28c177f93356c3c7c0324104a8695d3e
markbojack <markbojack.si@gmail.com>
2021-04-26 02:41:12 +0700

    update

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -10,6 +10,10 @@
 #   qute://help/configuring.html
 #   qute://help/settings.html
 
+
+## The page(s) to open at the start.
+c.url.start_pages = "https://startpage.com/"
+
 # Uncomment this to still load settings configured via autoconfig.yml
 # config.load_autoconfig()
 # Or uncomment this line to load settings from config.py
@@ -22,6 +26,12 @@ c.aliases = {'q': 'quit', 'w': 'session-save', 'wq': 'quit --save'}
 
 # Setting dark mode
 config.set("colors.webpage.darkmode.enabled", True)
+
+# Hide statusbar/tabs by default
+c.statusbar.show = "never"
+c.tabs.show = "never"
+#config.set("statusbar.show", "never")
+#config.set("tabs.show", "never")
 
 # Which cookies to accept. With QtWebEngine, this setting also controls
 # other features with tracking capabilities similar to those of cookies;
@@ -185,11 +195,12 @@ c.downloads.location.directory = '~/Downloads'
 #   - never: Always hide the tab bar.
 #   - multiple: Hide the tab bar if only one tab is open.
 #   - switching: Show the tab bar when switching tabs.
-c.tabs.show = 'always'
+#c.tabs.show = 'always'
 
 # Setting default page for when opening new tabs or new windows with
 # commands like :open -t and :open -w .
-c.url.default_page = 'file://~/.surf/html/homepage.html'
+#c.url.default_page = 'file://~/.surf/html/homepage.html'
+c.url.default_page = 'https://startpage.com'
 
 # Search engines which can be used via the address bar.  Maps a search
 # engine name (such as `DEFAULT`, or `ddg`) to a URL with a `{}`
@@ -344,22 +355,22 @@ c.colors.tabs.pinned.selected.even.bg = '#282c34'
 # font setting, it's replaced with the fonts listed here. If set to an
 # empty value, a system-specific monospace default is used.
 # Type: List of Font, or Font
-c.fonts.default_family = '"SauceCodePro Nerd Font"'
+c.fonts.default_family = '"Roboto Mono Nerd Font"'
 
 # Default font size to use. Whenever "default_size" is used in a font
 # setting, it's replaced with the size listed here. Valid values are
 # either a float value with a "pt" suffix, or an integer value with a
 # "px" suffix.
 # Type: String
-c.fonts.default_size = '11pt'
+c.fonts.default_size = '8pt'
 
 # Font used in the completion widget.
 # Type: Font
-c.fonts.completion.entry = '11pt "SauceCodePro Nerd Font"'
+c.fonts.completion.entry = '8pt "Roboto Mono Nerd Font"'
 
 # Font used for the debugging console.
 # Type: Font
-c.fonts.debug_console = '11pt "SauceCodePro Nerd Font"'
+c.fonts.debug_console = '8pt "Roboto Mono Nerd Font"'
 
 # Font used for prompts.
 # Type: Font
@@ -367,7 +378,7 @@ c.fonts.prompts = 'default_size sans-serif'
 
 # Font used in the statusbar.
 # Type: Font
-c.fonts.statusbar = '11pt "SauceCodePro Nerd Font"'
+c.fonts.statusbar = '8pt "Roboto Mono Nerd Font"'
 
 # Bindings to use dmenu rather than qutebrowser's builtin search.
 #config.bind('o', 'spawn --userscript dmenu-open')
@@ -388,3 +399,227 @@ config.bind(',dr', 'config-cycle content.user_stylesheets ~/solarized-everything
 config.bind(',gr', 'config-cycle content.user_stylesheets ~/solarized-everything-css/css/gruvbox/gruvbox-all-sites.css ""')
 config.bind(',sd', 'config-cycle content.user_stylesheets ~/solarized-everything-css/css/solarized-dark/solarized-dark-all-sites.css ""')
 config.bind(',sl', 'config-cycle content.user_stylesheets ~/solarized-everything-css/css/solarized-light/solarized-light-all-sites.css ""')
+
+
+# some rando's config.py ======================================================================
+# nah actually he's cool
+# https://gist.github.com/Ape/d0c48b3f7ec9c8efaecf48eaa1e75d0d
+#import setproctitle
+#setproctitle.setproctitle("qutebrowser")
+#
+## Bindings
+#config.bind("gi", "hint inputs")
+#config.bind("<f12>", "inspector")
+#
+#config.unbind("+")
+#config.unbind("-")
+#config.unbind("=")
+#config.bind("z+", "zoom-in")
+#config.bind("z-", "zoom-out")
+#config.bind("zz", "zoom")
+#
+#config.unbind("O")
+#config.unbind("T")
+#config.unbind("th")
+#config.unbind("tl")
+#config.bind("O", "set-cmd-text :open {url:pretty}")
+#config.bind("T", "set-cmd-text :open -t {url:pretty}")
+#config.bind("t", "set-cmd-text -s :open -t")
+#
+#config.unbind("<ctrl+tab>")
+#config.bind("<ctrl+tab>", "tab-next")
+#config.bind("<ctrl+shift+tab>", "tab-prev")
+#
+#config.unbind("ZQ")
+#config.unbind("ZZ")
+#config.unbind("<ctrl+q>")
+#config.bind("<ctrl+q>", "wq")
+#
+## Aliases for commands. The keys of the given dictionary are the
+## aliases, while the values are the commands they map to.
+#c.aliases = {
+    #"w": "session-save",
+    #"wq": "quit --save",
+    #"mpv": "spawn -d mpv --force-window=immediate {url}",
+    #"nicehash": "spawn --userscript nicehash",
+    #"pass": "spawn -d pass -c",
+#}
+#
+## Always restore open sites when qutebrowser is reopened.
+#c.auto_save.session = True
+#
+## Foreground color of the URL in the statusbar on successful load
+## (https).
+#c.colors.statusbar.url.success.https.fg = "white"
+#
+## Background color of unselected tabs.
+#c.colors.tabs.even.bg = "silver"
+#c.colors.tabs.odd.bg = "gainsboro"
+#
+## Foreground color of unselected tabs.
+#c.colors.tabs.even.fg = "#666666"
+#c.colors.tabs.odd.fg = c.colors.tabs.even.fg
+#
+## The height of the completion, in px or as percentage of the window.
+#c.completion.height = "20%"
+#
+## Move on to the next part when there's only one possible completion
+## left.
+#c.completion.quick = False
+#
+## When to show the autocompletion window.
+## Valid values:
+##   - always: Whenever a completion is available.
+##   - auto: Whenever a completion is requested.
+##   - never: Never.
+#c.completion.show = "auto"
+#
+## Whether quitting the application requires a confirmation.
+## Valid values:
+##   - always: Always show a confirmation.
+##   - multiple-tabs: Show a confirmation if multiple tabs are opened.
+##   - downloads: Show a confirmation if downloads are running
+##   - never: Never show a confirmation.
+#c.confirm_quit = ["downloads"]
+#
+## Value to send in the `Accept-Language` header.
+#c.content.headers.accept_language = "en-US,en;q=0.8,fi;q=0.6"
+#
+## The proxy to use. In addition to the listed values, you can use a
+## `socks://...` or `http://...` URL.
+## Valid values:
+##   - system: Use the system wide proxy.
+##   - none: Don"t use any proxy
+#c.content.proxy = "none"
+#
+## Validate SSL handshakes.
+## Valid values:
+##   - true
+##   - false
+##   - ask
+#c.content.ssl_strict = True
+#
+## A list of user stylesheet filenames to use.
+#c.content.user_stylesheets = "user.css"
+#
+## The directory to save downloads to. If unset, a sensible os-specific
+## default is used.
+#c.downloads.location.directory = "/tmp/ape"
+#
+## Prompt the user for the download location. If set to false,
+## `downloads.location.directory` will be used.
+#c.downloads.location.prompt = False
+#
+## The editor (and arguments) to use for the `open-editor` command. `{}`
+## gets replaced by the filename of the file to be edited.
+#c.editor.command = ["termite", "-e", "vim '{}'"]
+#
+#monospace = "8px 'Bok MonteCarlo'"
+#
+## Font used in the completion categories.
+#c.fonts.completion.category = f"bold {monospace}"
+#
+## Font used in the completion widget.
+#c.fonts.completion.entry = monospace
+#
+## Font used for the debugging console.
+#c.fonts.debug_console = monospace
+#
+## Font used for the downloadbar.
+#c.fonts.downloads = monospace
+#
+## Font used in the keyhint widget.
+#c.fonts.keyhint = monospace
+#
+## Font used for error messages.
+#c.fonts.messages.error = monospace
+#
+## Font used for info messages.
+#c.fonts.messages.info = monospace
+#
+## Font used for warning messages.
+#c.fonts.messages.warning = monospace
+#
+## Font used for prompts.
+#c.fonts.prompts = monospace
+#
+## Font used in the statusbar.
+#c.fonts.statusbar = monospace
+#
+## Font used in the tab bar.
+#c.fonts.tabs = monospace
+#
+## Font used for the hints.
+#c.fonts.hints = "bold 13px 'DejaVu Sans Mono'"
+#
+## Chars used for hint strings.
+#c.hints.chars = "asdfghjklie"
+#
+## Leave insert mode if a non-editable element is clicked.
+#c.input.insert_mode.auto_leave = True
+#
+## Automatically enter insert mode if an editable element is focused
+## after loading the page.
+#c.input.insert_mode.auto_load = True
+#
+## Show a scrollbar.
+c.scrolling.bar = "never"
+#
+## Enable smooth scrolling for web pages. Note smooth scrolling does not
+## work with the `:scroll-px` command.
+#c.scrolling.smooth = False
+#
+## Open new tabs (middleclick/ctrl+click) in the background.
+#c.tabs.background = True
+#
+## Behavior when the last tab is closed.
+## Valid values:
+##   - ignore: Don't do anything.
+##   - blank: Load a blank page.
+##   - startpage: Load the start page.
+##   - default-page: Load the default page.
+##   - close: Close the window.
+#c.tabs.last_close = "close"
+#
+## Padding around text for tabs
+#c.tabs.padding = {
+    #"left": 5,
+    #"right": 5,
+    #"top": 0,
+    #"bottom": 1,
+#}
+#
+## Which tab to select when the focused tab is removed.
+## Valid values:
+##   - prev: Select the tab which came before the closed one (left in horizontal, above in vertical).
+##   - next: Select the tab which came after the closed one (right in horizontal, below in vertical).
+##   - last-used: Select the previously selected tab.
+#c.tabs.select_on_remove = "prev"
+#
+## Width of the progress indicator (0 to disable).
+#c.tabs.width.indicator = 0
+#
+#
+## Definitions of search engines which can be used via the address bar.
+## Maps a searchengine name (such as `DEFAULT`, or `ddg`) to a URL with a
+## `{}` placeholder. The placeholder will be replaced by the search term,
+## use `{{` and `}}` for literal `{`/`}` signs. The searchengine named
+## `DEFAULT` is used when `url.auto_search` is turned on and something
+## else than a URL was entered to be opened. Other search engines can be
+## used by prepending the search engine name to the search term, e.g.
+## `:open google qutebrowser`.
+#c.url.searchengines = {"DEFAULT": "https://www.google.fi/search?q={}"}
+#
+#
+## The format to use for the window title. The following placeholders are
+## defined:
+##   * `{perc}`: The percentage as a string like `[10%]`.
+##   * `{perc_raw}`: The raw percentage, e.g. `10`
+##   * `{title}`: The title of the current web page
+##   * `{title_sep}`: The string ` - ` if a title is set, empty otherwise.
+##   * `{id}`: The internal window ID of this window.
+##   * `{scroll_pos}`: The page scroll position.
+##   * `{host}`: The host of the current web page.
+##   * `{backend}`: Either ''webkit'' or ''webengine''
+##   * `{private}` : Indicates when private mode is enabled.
+#c.window.title_format = "{private}{perc}{title}{title_sep}qutebrowser"
