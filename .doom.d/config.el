36de98874d7d8350d72c8cdc27aceb0cfe955208
markbojack <markbojack.si@gmail.com>
2021-06-26 12:00:01 +0700

    update

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -64,6 +64,7 @@
 
 ;; ORG -------------------------------------------------------------------------
 (setq org-link-search-must-match-exact-headline nil)    ;; to target words with a link
+
 ;; ORG SUPER AGENDA ------------------------------------------------------------
 ;; https://github.com/alphapapa/org-super-agenda
 (use-package! org-super-agenda
@@ -84,6 +85,71 @@
                                    :tag "bo")))
   :config
   (org-super-agenda-mode))
+
+(add-hook! 'org-mode-hook #'+org-pretty-mode #'mixed-pitch-mode)
+(add-hook! 'org-mode-hook (company-mode -1))
+(add-hook! 'org-capture-mode-hook (company-mode -1))
+
+(setq org-ellipsis " ▾ "
+      org-bullets-bullet-list '("·")
+      org-tags-column -80
+      org-agenda-files (ignore-errors (directory-files +org-dir t "\\.org$" t))
+      org-log-done 'time
+      css-indent-offset 2
+      org-refile-targets (quote ((nil :maxlevel . 1))))
+      ;org-capture-templates '(("x" "Note" entry
+                               ;(file+olp+datetree "journal.org")
+                               ;"**** [ ] %U %?" :prepend t :kill-buffer t)
+                              ;("t" "Task" entry
+                               ;(file+headline "tasks.org" "Inbox")
+                               ;"* [ ] %?\n%i" :prepend t :kill-buffer t)))
+
+(after! org
+  (set-face-attribute 'org-link nil
+                      :weight 'normal
+                      :background nil)
+  (set-face-attribute 'org-code nil
+                      :foreground "#a9a1e1"
+                      :background nil)
+  (set-face-attribute 'org-date nil
+                      :foreground "#5B6268"
+                      :background nil)
+  (set-face-attribute 'org-level-1 nil
+                      :foreground "steelblue2"
+                      :background nil
+                      :height 1.2
+                      :weight 'normal)
+  (set-face-attribute 'org-level-2 nil
+                      :foreground "slategray2"
+                      :background nil
+                      :height 1.0
+                      :weight 'normal)
+  (set-face-attribute 'org-level-3 nil
+                      :foreground "SkyBlue2"
+                      :background nil
+                      :height 1.0
+                      :weight 'normal)
+  (set-face-attribute 'org-level-4 nil
+                      :foreground "DodgerBlue2"
+                      :background nil
+                      :height 1.0
+                      :weight 'normal)
+  (set-face-attribute 'org-level-5 nil
+                      :weight 'normal)
+  (set-face-attribute 'org-level-6 nil
+                      :weight 'normal)
+  (set-face-attribute 'org-document-title nil
+                      :foreground "SlateGray1"
+                      :background nil
+                      :height 1.75
+                      :weight 'bold)
+
+(setq org-fancy-priorities-list '("⚡" "⬆" "⬇" "☕")))
+
+;(setq +org-capture-todo-file "tasks.org")
+;(set-popup-rule! "^\\*Org Agenda" :side 'bottom :size 0.90 :select t :ttl nil)
+;(set-popup-rule! "^CAPTURE.*\\.org$" :side 'bottom :size 0.90 :select t :ttl nil)
+;(set-popup-rule! "^\\*org-brain" :side 'right :size 1.00 :select t :ttl nil)
 
 ;; NOTES =======================================================================
 
